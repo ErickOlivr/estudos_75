c2c23d21d47e769cd07fd7d8489f6f926664d684
Erick Oliveira <166569357+ErickOlivr@users.noreply.github.com>
2025-03-30 23:16:50 -0300

    Add files via upload

--- matriz.py
+++ matriz.py
@@ -1,5 +1,6 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-p = 0
+som =soml= 0
+maior = []
 for l in range(0,3):
     for c in range(0,3):
         matriz[l][c] = int(input(f"digite um valor para [{l}, {c}]: "))
@@ -7,4 +8,14 @@ print("#" * 30)
 for l in range(0,3):
     for c in range(0,3):
         print(f"[{matriz[l][c]:^5}]", end="")
-    print()
+        if matriz [l][c]%2 == 0:
+            som += matriz[l][c]
+    print()        
+print(som)
+soml = matriz[0][1] + matriz[1][1] + matriz[2][1]
+print(soml)
+maior.append(matriz[0][1])
+maior.append(matriz[1][1])
+maior.append(matriz[2][1])
+maior.sort()
+print(maior[2])
